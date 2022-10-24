8d316eff90d81ad1d5c2df2fa1df2a71d93b00eb
NorSP <norbissilvera@gmail.com>
2022-10-24 18:07:32 +0000

    hola

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, json
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
@@ -33,10 +33,63 @@ def handle_hello():
     response_body = {
         "hello": "world",
         "family": members
+        
     }
 
 
     return jsonify(response_body), 200
+
+@app.route("/members", methods=["POST"])
+def add_member():
+    first_name= request.json.get("first_name", None)
+    age = request.json.get("age", None) 
+    lucky_numbers= request.json.get("lucky_numbers", None)
+    new_members = {
+        "first_name": first_name,
+        "age": age,
+        "lucky_numbers":lucky_numbers
+        }
+    
+#crea el acceso y devuelve un token a las personas al loguearse
+    members = jackson_family.add_member(new_members)
+
+    
+    response_body={
+       
+       "msg":"New member added"
+        
+    }
+    return jsonify(response_body), 200
+
+@app.route("/member/<int:member_id>", methods=["GET"])
+def get_member(member_id):
+   
+
+    if jackson_family.get_member(member_id) != "":
+            
+        return jsonify({"member":jackson_family.get_member(member_id)}), 200
+    elif jackson_family.get_member(member_id) == "":
+        return jsonify({"msg":"member not found"}), 404
+    
+    return jsonify({"msg":"member not found"}), 404
+
+@app.route("/members", methods=["DELETE"])
+def delete_member():
+    body=json.loads(request.data)
+    if body is not None:
+        key="id"
+        if key in body:
+            if jackson_family.delete_member(body["id"]) == True:
+               return jsonify({ "done": "True"}), 200 
+            else:
+                return jsonify({"msg":"member not found"}), 404
+            
+                     
+    return jsonify({"msg":"Action not acepted"}), 400
+
+
+    
+   
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -7,30 +7,90 @@ update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 from random import randint
+import json
+from flask import Flask, request, jsonify, url_for
 
 class FamilyStructure:
+
     def __init__(self, last_name):
+        # Recibe un String, corresponde al apellido de la familia.
         self.last_name = last_name
 
-        # example list of members
-        self._members = []
+        # Se guardan los miembros
+        self._members = [{
+            "id":self._generateId(),
+            "first_name": "John",
+             "last_name": last_name,
+             "age": "33 years old",
+             "lucky_numbers": [7, 13, 22]
+        },{
+            "id":self._generateId(),
+            "first_name": "Jane",
+             "last_name": last_name,
+             "age":"35 years old",
+             "lucky_numbers": [10, 14, 3]
+        },{
+            "id":self._generateId(),
+            "first_name": "Jimmy",
+             "last_name": last_name,
+             "age": "5 years old",
+             "lucky_numbers": [1]
+        }]
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+    # Se genera el ID de forma random de los miembros
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # Agregar un miembro de la familia
+
+
+        return self._members.append({
+            "id":self._generateId(),
+            "first_name": member["first_name"],
+             "last_name": self.last_name,
+             "age": str(member["age"])+" years old",
+             "lucky_numbers": member["lucky_numbers"]
+        })
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        # Borrar un miembro de la familia
+        num = -1
+            
+            
+        for content in self._members:
+            num = num+1
+            bool = False
+               
+            if content["id"]== id:
+                self._members.pop(num)
+                bool = True
+                break
+        
+        return bool
+               
+                  
+            
+
+                    
+                
+            
+     # Obtener un miembro de la familia            
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        num = -1
+        obj = ""
+        for content in self._members:
+            num = num+1
+            
+            if content["id"]== id:
+                obj = self._members[num]
+                bool = True
+                break
+        return obj    
 
-    # this method is done, it returns a list with all the family members
+    # Lista con todos los miembros de la familia
     def get_all_members(self):
         return self._members
+
+    
